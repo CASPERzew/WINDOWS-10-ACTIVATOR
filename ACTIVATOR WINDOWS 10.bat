d6f8ad2512d7a530ea76d81b540e9e5e1d850d4c
CASPERzew <69422046+CASPERzew@users.noreply.github.com>
2024-06-30 19:13:50 +0300

    Add files via upload

---
+++ ACTIVATOR WINDOWS 10.bat
@@ -0,0 +1,145 @@
+@echo off
+TITLE WONDOWS 10 ACTIVATOR [BY CASPERZEW]
+ECHO              db     dP""b8 888888 88 Yb    dP    db    888888  dP"Yb  88""Yb 
+ECHO             dPYb   dP   `"   88   88  Yb  dP    dPYb     88   dP   Yb 88__dP 
+ECHO            dP__Yb  Yb        88   88   YbdP    dP__Yb    88   Yb   dP 88"Yb  
+ECHO           dP""""Yb  YboodP   88   88    YP    dP""""Yb   88    YbodP  88  Yb 
+echo.
+:menu
+echo.
+echo SELECT YOUR WINDOWS
+echo 1.  HOME
+echo 2.  HOME N
+echo 3.  HOME SINGLE LANGUAGE
+echo 4 . HOME COUNTRY SPECIFIC
+echo 5.  PROFESSIONAL
+echo 6.  PROFESSIONAL N
+echo 7.  EDUCATION
+echo 8.  EDUCATION N
+echo 9.  ENTERPRISE
+echo 10. ENTERPRISE N
+echo.
+set /p choice="SELECTION: "
+
+if "%choice%"=="1" (
+    cls
+ECHO 88  88  dP"Yb  8b    d8 888888 
+ECHO 88  88 dP   Yb 88b  d88 88__   
+ECHO 888888 Yb   dP 88YbdP88 88""   
+ECHO 88  88  YbodP  88 YY 88 888888 
+    slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+
+if "%choice%"=="2" (
+    CLS
+ECHO 88  88  dP"Yb  8b    d8 888888     88b 88 
+ECHO 88  88 dP   Yb 88b  d88 88__       88Yb88 
+ECHO 888888 Yb   dP 88YbdP88 88""       88 Y88 
+ECHO 88  88  YbodP  88 YY 88 888888     88  Y8 
+    slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="3" (
+     CLS
+ECHO .dP"Y8 88 88b 88  dP""b8 88     888888     88        db    88b 88  dP""b8 88   88    db     dP""b8 888888 
+ECHO `Ybo." 88 88Yb88 dP   `" 88     88__       88       dPYb   88Yb88 dP   `" 88   88   dPYb   dP   `" 88__   
+ECHO o.`Y8b 88 88 Y88 Yb  "88 88  .o 88""       88  .o  dP__Yb  88 Y88 Yb  "88 Y8   8P  dP__Yb  Yb  "88 88""   
+ECHO 8bodP' 88 88  Y8  YboodP 88ood8 888888     88ood8 dP""""Yb 88  Y8  YboodP `YbodP' dP""""Yb  YboodP 888888 
+    slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="4" (
+CLS
+ECHO  dP""b8  dP"Yb  88   88 88b 88 888888 88""Yb Yb  dP     .dP"Y8 88""Yb 888888  dP""b8 88 888888 88  dP""b8 
+ECHO dP   `" dP   Yb 88   88 88Yb88   88   88__dP  YbdP      `Ybo." 88__dP 88__   dP   `" 88 88__   88 dP   `" 
+ECHO Yb      Yb   dP Y8   8P 88 Y88   88   88"Yb    8P       o.`Y8b 88"""  88""   Yb      88 88""   88 Yb      
+ECHO  YboodP  YbodP  `YbodP' 88  Y8   88   88  Yb  dP        8bodP' 88     888888  YboodP 88 88     88  YboodP 
+    slmgr /ipk PVMJN-6DFY6–9CCP6–7BKTT-D3WVR
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="5" (
+CLS
+ECHO 88""Yb 88""Yb  dP"Yb  888888 888888 .dP"Y8 .dP"Y8 88  dP"Yb  88b 88    db    88     
+ECHO 88__dP 88__dP dP   Yb 88__   88__   `Ybo." `Ybo." 88 dP   Yb 88Yb88   dPYb   88     
+ECHO 88"""  88"Yb  Yb   dP 88""   88""   o.`Y8b o.`Y8b 88 Yb   dP 88 Y88  dP__Yb  88  .o 
+ECHO 88     88  Yb  YbodP  88     888888 8bodP' 8bodP' 88  YbodP  88  Y8 dP""""Yb 88ood8 
+    slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="6" (
+CLS
+ECHO 88""Yb 88""Yb  dP"Yb  888888 888888 .dP"Y8 .dP"Y8 88  dP"Yb  88b 88    db    88         88b 88 
+ECHO 88__dP 88__dP dP   Yb 88__   88__   `Ybo." `Ybo." 88 dP   Yb 88Yb88   dPYb   88         88Yb88 
+ECHO 88"""  88"Yb  Yb   dP 88""   88""   o.`Y8b o.`Y8b 88 Yb   dP 88 Y88  dP__Yb  88  .o     88 Y88 
+ECHO 88     88  Yb  YbodP  88     888888 8bodP' 8bodP' 88  YbodP  88  Y8 dP""""Yb 88ood8     88  Y8 
+    slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="7" (
+CLS
+ECHO 888888 8888b.  88   88  dP""b8    db    888888 88  dP"Yb  88b 88 
+ECHO 88__    8I  Yb 88   88 dP   `"   dPYb     88   88 dP   Yb 88Yb88 
+ECHO 88""    8I  dY Y8   8P Yb       dP__Yb    88   88 Yb   dP 88 Y88 
+ECHO 888888 8888Y"  `YbodP'  YboodP dP""""Yb   88   88  YbodP  88  Y8 
+    slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="8" (
+CLS
+ECHO 888888 8888b.  88   88  dP""b8    db    888888 88  dP"Yb  88b 88     88b 88 
+ECHO 88__    8I  Yb 88   88 dP   `"   dPYb     88   88 dP   Yb 88Yb88     88Yb88 
+ECHO 88""    8I  dY Y8   8P Yb       dP__Yb    88   88 Yb   dP 88 Y88     88 Y88 
+ECHO 888888 8888Y"  `YbodP'  YboodP dP""""Yb   88   88  YbodP  88  Y8     88  Y8 
+    slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="9" (
+CLS
+ECHO 888888 88b 88 888888 888888 88""Yb 88""Yb 88""Yb 88 .dP"Y8 888888 
+ECHO 88__   88Yb88   88   88__   88__dP 88__dP 88__dP 88 `Ybo." 88__   
+ECHO 88""   88 Y88   88   88""   88"Yb  88"""  88"Yb  88 o.`Y8b 88""   
+ECHO 888888 88  Y8   88   888888 88  Yb 88     88  Yb 88 8bodP' 888888 
+    slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+if "%choice%"=="10" (
+CLS
+ECHO 888888 88b 88 888888 888888 88""Yb 88""Yb 88""Yb 88 .dP"Y8 888888     88b 88 
+ECHO 88__   88Yb88   88   88__   88__dP 88__dP 88__dP 88 `Ybo." 88__       88Yb88 
+ECHO 88""   88 Y88   88   88""   88"Yb  88"""  88"Yb  88 o.`Y8b 88""       88 Y88 
+ECHO 888888 88  Y8   88   888888 88  Yb 88     88  Yb 88 8bodP' 888888     88  Y8 
+    slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+    timeout 3
+    slmgr /skms kms8.msguides.com
+    timeout 3
+    slmgr /ato
+)
+pause
